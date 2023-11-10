a1dca79f66e8f659b7c84021bef8d7b547a0813c
ayushpasi <pasiayush999@gmail.com>
2023-11-10 19:04:37 +0530

    bycrpt encription added

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,5 @@
 const UserModel = require("../models/userModel");
+const bcrypt = require("bcrypt");
 const path = require("path");
 
 const getLoginPage = async (req, res, next) => {
@@ -24,15 +25,17 @@ const postUserSignUp = async (req, res) => {
         error: "This email is already taken. Please choose another one.",
       });
     }
+    bcrypt.hash(password, 10, async (err, hash) => {
+      console.log(err);
+      // Only create a new user if all required data is present and the email doesn't exist
+      const newUser = await UserModel.create({
+        name,
+        email,
+        password: hash,
+      });
 
-    // Only create a new user if all required data is present and the email doesn't exist
-    const newUser = await UserModel.create({
-      name,
-      email,
-      password,
+      res.status(200).json({ message: "Registered successfully!" });
     });
-
-    res.status(200).json({ message: "Registered successfully!" });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Internal server error" });
@@ -44,11 +47,16 @@ const postUserLogin = async (req, res) => {
     const existingUser = await UserModel.findOne({ where: { email: email } });
     if (existingUser) {
       console.log(existingUser);
-      if (existingUser.password === password) {
-        res.send(`alert("login succesfully")`);
-      } else {
-        res.status(401).json({ errr: "User not authorized" });
-      }
+      bcrypt.compare(password, existingUser.password, (err, result) => {
+        if (err) {
+          res.status(500).json({ error: "somthing went wrong" });
+        }
+        if (result == true) {
+          res.status(200).send(`alert("login succesfully")`);
+        } else {
+          res.status(401).json({ error: "User not authorized" });
+        }
+      });
     } else {
       res.status(404).json({ error: "User Not Found" });
     }
